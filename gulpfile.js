5f7caf039cca71ad20194e64bd183283c6e105da
CodeBlack29 <39704797+vegas29@users.noreply.github.com>
2021-12-11 10:07:34 -0500

    header, lsito

--- gulpfile.js
+++ gulpfile.js
@@ -57,4 +57,4 @@ exports.dev = dev;
 exports.imagenes = imagenes;
 exports.versionWebp = versionWebp;
 exports.versionAvif = versionAvif;
-exports.default = series( imagenes, versionWebp, versionAvif, css, dev  );
+exports.default = series(css, dev  );
